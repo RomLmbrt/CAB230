d8307f3bbcb912594fcd06b65897b5489a265afa
RomLmbrt <101237437+RomLmbrt@users.noreply.github.com>
2022-05-12 19:49:45 +1000

    Add files via upload

---
+++ Assignment 1/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <main>
+      {/* icon */}
+      <div id="icon">
+        <img src="img/volcano.png" alt="volcano" />
+      </div>
+    </main>
+  );
+}
