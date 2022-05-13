d7ac016960f5e44e8b8cbb2bb8bed1d8aaa678e1
RomLmbrt <101237437+RomLmbrt@users.noreply.github.com>
2022-05-13 10:58:34 +1000

    Add files via upload

---
+++ assignment-1/src/api.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = "";
+
+export function useVolcanoList() {
+  const [loading, setLoading] = useState(true);
+  const [rowData, setRowData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getVolcanoList()
+      .then((volcanoList) => {
+        setRowData(volcanoList);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return {
+    loading,
+    rowData,
+    error
+  };
+}
+
+function getVolcanoList() {
+  return fetch("https://openlibrary.org/subjects/drama.json?published_in=2000")
+      .then((res) => res.json())
+      .then((data) => data.works)
+      .then((works) =>
+        works.map((work) => {
+          return {
+            title: work.title,
+            author: work.authors[0].name,
+            editionCount: work.edition_count,
+            id: work.cover_id
+          };
+        })
+      )
+  }
