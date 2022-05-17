077dd92e232c1e65006f35c4e969809921c186ab
RomLmbrt <101237437+RomLmbrt@users.noreply.github.com>
2022-05-17 17:44:31 +1000

    Update Register.jsx

--- Assignment 1/src/pages/Register.jsx
+++ Assignment 1/src/pages/Register.jsx
@@ -4,25 +4,17 @@ import { useState, useEffect } from "react";
 
 export default function Register() {
   var [countries, setCountries] = useState([]);
-/*
+
   useEffect(() => {
       fetch(`http://sefdb02.qut.edu.au:3001/countries`)
       .then((res) => res.json())
       .then((volcanoList) => {
         setCountries(volcanoList);
       });
-  }, []);*/
-
-  const Http = new XMLHttpRequest();
-  const url='http://sefdb02.qut.edu.au:3001/countries';
-  Http.open("GET", url);
-  Http.responseType = 'list';
-  Http.onload = function() {
-    setCountries(Http.response);
-  };
-  Http.send();
+  }, []);
 
   console.log(countries);
+
   return ( 
     <main>
       <h1>{}</h1>
